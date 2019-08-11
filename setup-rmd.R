8a30138d7bf4293c2f510d608d5dfa881c9b31c4
tereom <teresa.ortiz.mancera@gmail.com>
2019-08-11 17:17:07 -0500

    Add setup file to be run prior to rmds

---
+++ setup-rmd.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+knitr::opts_chunk$set(
+    comment = "#>",
+    collapse = TRUE,
+    fig.align = "center"
+)
+comma <- function(x) format(x, digits = 2, big.mark = ",")
+ggplot2::theme_set(ggplot2::theme_minimal())
+if(!require(estcomp)) devtools::install_github("tereom/estcomp")
