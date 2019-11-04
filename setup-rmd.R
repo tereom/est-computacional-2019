6ce9bef3332839149cfb1fa88beebb090f0d4c2b
tereom <teresa.ortiz.mancera@gmail.com>
2019-11-04 11:27:58 -0600

    Bayes

--- setup-rmd.R
+++ setup-rmd.R
@@ -2,7 +2,8 @@ library(tidyverse)
 knitr::opts_chunk$set(
     comment = "#>",
     collapse = TRUE,
-    fig.align = "center"
+    fig.align = "center", 
+    error = FALSE
 )
 comma <- function(x) format(x, digits = 2, big.mark = ",")
 ggplot2::theme_set(ggplot2::theme_light())
